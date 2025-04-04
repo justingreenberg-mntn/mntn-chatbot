533176c69ab3bdb7c7f9ac90a2b0420614ca3fa4
jgreenberg <jgreenberg@mountain.com>
2025-04-04 10:31:47 -0700

    WIP

--- src/main/java/com/mntn/contextchat/model/JiraProjectDTO.java
+++ src/main/java/com/mntn/contextchat/model/JiraProjectDTO.java
@@ -2,6 +2,7 @@ package com.mntn.contextchat.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
 
 import java.util.Map;
 
@@ -20,11 +21,4 @@ public class JiraProjectDTO {
 
     @JsonProperty("avatarUrls")
     private Map<String, String> avatarUrls;
-
-    // Getters and Setters
-    public String getId() {
-        return id;
-    }
-
-    public void
-
+}
