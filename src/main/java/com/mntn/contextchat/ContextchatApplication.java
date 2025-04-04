f52782d4e358167c1fcd32ea5daef495be8d1b57
jgreenberg <jgreenberg@mountain.com>
2025-04-04 10:00:30 -0700

    initial commit

---
+++ src/main/java/com/mntn/contextchat/ContextchatApplication.java
@@ -0,0 +1,13 @@
+package com.mntn.contextchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContextchatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContextchatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mntn/contextchat/ContextchatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mntn.contextchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContextchatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
